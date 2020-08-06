cc136b701f9d615041b9016e01c51beb724615c9
Mark Branscum <mbstudios@hotmail.com>
2020-08-05 20:22:42 -0500

    Finished code for search

--- js/app.js
+++ js/app.js
@@ -2,4 +2,26 @@ baguetteBox.run(".gallery", {
    animation: "slideIn",
    buttons: true
 });
-// const search = new Filter("search", "data-caption");
+
+document.querySelector("#search").addEventListener("keyup", userSearch);
+
+function userSearch() {
+   const listCnt = document.querySelectorAll("a[data-caption]");
+   let captionsList = [];
+
+   for (let i = 0; i < listCnt.length; i++) {
+      let captions = listCnt[i].getAttribute("data-caption");
+      captionsList.push(captions.toLowerCase());
+      // create variable set it to user search input toLowerCase()
+      let searchVar = document.querySelector("#search");
+      searchVar = searchVar.value.toLowerCase();
+      // create conditional statement (if) to search indexOf
+      if (captionsList[i].indexOf(searchVar) > -1) {
+         // set style of lstCnt to display block
+         listCnt[i].style.display = "block";
+      } else {
+         // set style of lstCnt to display none
+         listCnt[i].style.display = "none";
+      }
+   }
+}
